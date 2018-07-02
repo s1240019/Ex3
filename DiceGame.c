8d7e7b7668baf62d41f20e3fd9577f34bd341c66
s1240019 <s1240019@example.com>
2018-07-02 17:08:46 +0900

    Initial revision

--- DiceGame.c
+++ DiceGame.c
@@ -6,8 +6,8 @@ int main(){
   int d1,d2,sum;
   srand((unsigned)time(NULL));
   printf("Rolling the dice...\n");
-  d1=rand()%5+1;
-  d2=rand()%5+1;
+  d1=rand()%6+1;
+  d2=rand()%6+1;
   printf("Die 1: %d\n",d1);
   printf("Die 2: %d\n",d2);
   sum=d1+d2;
