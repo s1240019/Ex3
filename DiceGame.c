7344d76b78e62035c22d8fca6200feb60e264258
s1240019 <s1240019@example.com>
2018-07-02 17:06:24 +0900

    Greetings added

--- DiceGame.c
+++ DiceGame.c
@@ -1,13 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
-
+#define N 10
 int main(){
   int d1,d2,sum;
+  char name[N];
+  
+  printf("What is your name?\n");
+  scanf("%s",name);
+  printf("Hello, %s!\n",name);
+  
+  
   srand((unsigned)time(NULL));
   printf("Rolling the dice...\n");
-  d1=rand()%5+1;
-  d2=rand()%5+1;
+  d1=rand()%6+1;
+  d2=rand()%6+1;
   printf("Die 1: %d\n",d1);
   printf("Die 2: %d\n",d2);
   sum=d1+d2;
