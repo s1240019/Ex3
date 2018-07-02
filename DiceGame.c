c1c0fe7c5b881e4ae1b4752e54ea5d0b9808fa98
s1240019 <s1240019@example.com>
2018-07-02 16:25:23 +0900

    Initial revision

---
+++ DiceGame.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int main(){
+  int d1,d2,sum;
+  srand((unsigned)time(NULL));
+  printf("Rolling the dice...\n");
+  d1=rand()%5+1;
+  d2=rand()%5+1;
+  printf("Die 1: %d\n",d1);
+  printf("Die 2: %d\n",d2);
+  sum=d1+d2;
+  printf("Total value: %d\n",sum);
+  return 0;
+}
+  
+  
